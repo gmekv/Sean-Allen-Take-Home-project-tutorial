d8e0e7fc2c04593a0e69c7411e182a8c4ddc8c47
Giorgi <giorgi.mekvabishvili@protonmail.com>
2026-04-09 20:26:17 +0400

    Add user header view controller and reorganize label classes

---
+++ GHFollowers/Custom Views/ViewControllers/GFUserInfoHeaderVC.swift
@@ -0,0 +1,51 @@
+//
+//  GFUserInfoHeaderVC.swift
+//  GHFollowers
+//
+//  Created by Giorgi Mekvabishvili on 27.03.26.
+//
+
+import UIKit
+
+class GFUserInfoHeaderVC: UIViewController {
+    
+    let avatarImageView = GFAvatarImageView(frame: .zero)
+    let usernameLabel = GFTitleLabel(textAlignment: .left, fontSize: 34)
+    let nameLabel  = GFSecondaryTitleLabel(fontSize: 18)
+    let locationImageView = UIImageView()
+    let locationLabel = GFSecondaryTitleLabel(fontSize: 18)
+    let bioLabel = GFBodyLabel(textAlignment: .left)
+    
+    var user: User!
+    
+    init(user: User!) {
+        super.init(nibName: nil, bundle: nil)
+        self.user = user
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    func addSubview() {
+        view.addSubview(avatarImageView)
+        view.addSubview(usernameLabel)
+        view.addSubview(nameLabel)
+        view.addSubview(locationLabel)
+        view.addSubview(locationImageView)
+        view.addSubview(bioLabel)
+    }
+    
+    func layoutUI(){
+        let padding: CGFloat = 20
+        let textImagePadding: CGFloat  = 12
+        locationImageView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+        
+        ])
+    }
+}
--- GHFollowers/Utilities/GFError.swift
+++ GHFollowers/Utilities/GFError.swift
@@ -1,4 +1,3 @@
-
 import Foundation
 
 enum GFError: String, Error {
